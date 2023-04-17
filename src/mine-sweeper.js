9ca5c450edf47e6c73956890b0485c1a4e92f13b
guchigow <lanov64@yandex.kz>
2023-04-18 03:41:01 +0600

    feat: +3 tasks (Tue Apr 18 03:41:00 AM +06 2023)

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function checkPosition(pos) {
+  if (pos !== undefined) {
+    return (pos) ? 1 : 0;
+  }
+  return 0;
+}
+
+function checkCurLevel(subArr, j) {
+  let bombsAround = 0;
+  bombsAround += checkPosition(subArr[j - 1]); //check left position
+  bombsAround += checkPosition(subArr[j + 1]); //check right position
+  return bombsAround;
+}
+
+function checkOtherLevel(level, j) {
+  let bombsAround = 0;
+  if (level !== undefined) {
+    bombsAround += (level[j]) ? 1 : 0;// check top or down
+    bombsAround += checkPosition(level[j - 1]); //left diagonal check
+    bombsAround += checkPosition(level[j + 1]); //right diagonal check
+  }
+  return bombsAround;
+}
+
+function checkSubArr(matrix, i) {
+  let resSubArr = [];
+  for (let j = 0; j < matrix[i].length; j++) {
+    let bombsAround = 0;
+    bombsAround += checkCurLevel(matrix[i], j); //check left and right position
+    bombsAround += checkOtherLevel(matrix[i - 1], j); // check top array
+    bombsAround += checkOtherLevel(matrix[i + 1], j); // check down array
+    resSubArr.push(bombsAround);
+  }
+  return resSubArr;
+}
+
+function minesweeper(matrix) {
+  let resMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let resSubArr = checkSubArr(matrix, i); // check current sub array
+    resMatrix.push(resSubArr); // result of sub array to result matrix
+  }
+  return resMatrix;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  /*
+    constructor() {
+      this.maxDeep = 1;
+    }
+
+    calculateOneArr(arr, curDeep) {
+      let res = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          res = this.calculateOneArr(arr[i], curDeep + 1);
+        }
+        if (res > this.maxDeep) {
+          this.maxDeep = res;
+        }
+      }
+      return curDeep;
+    }
+
+    calculateDepth(arr) {
+      let curDeep = 0;
+      if (arr.length === 0) {
+        return 1;
+      }
+      for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          this.calculateOneArr(arr[i], curDeep + 1);
+        }
+      }
+      return this.maxDeep + 1;
+    }
+  */
+
+  // constructor() {
+  //   this.maxDeep = 1;
+  // }
+
+  // calculateDepth(arr, curDeep = 1, first = true) {
+  //   let res = 0;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (Array.isArray(arr[i])) {
+  //       res = this.calculateDepth(arr[i], curDeep + 1, false);
+  //       if (res > this.maxDeep) {
+  //         this.maxDeep = res;
+  //       }
+  //     }
+  //   }
+  //   if (first) {
+  //     return this.maxDeep;
+  //   }
+  //   return curDeep;
+  // }
+
+  calculateDepth(arr) {
+    let maxDeep = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        let curDeep = 1 + this.calculateDepth(arr[i]);
+        if (curDeep > maxDeep) {
+          maxDeep = curDeep;
+        }
+      }
+    }
+    return maxDeep;
   }
 }
 
